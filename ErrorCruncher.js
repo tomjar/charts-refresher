8b3f10ce1dd082d6591432c99419f16c41275267
Thomas Reese <tronbuddy@zoho.com>
2020-04-11 12:23:34 -0500

    init commit

---
+++ ErrorCruncher.js
@@ -0,0 +1,91 @@
+var ErrorCruncher = {
+
+helloworld: function(){
+    alert('hello world!');
+},
+
+// pies/doughnuts
+// TODO: ErrorsByType
+// TODO: ErrorsByUser
+// TODO: ErrorsByDistrict
+
+// line
+// TODO: ErrorsByDay
+// TODO: ErrorsByMonth
+// TODO: ErrorsByHour? maybe
+
+
+showChart: function(canvasid, chartType){
+    var ctx = document.getElementById(canvasid).getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: chartType, // bar, line, pie
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
