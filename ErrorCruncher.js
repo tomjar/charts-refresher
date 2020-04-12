61fc115103bd1a4be19853067f6c5c18767bb104
Thomas Reese <cyclemadmot@gmail.com>
2020-04-11 21:58:43 -0500

    more work

--- ErrorCruncher.js
+++ ErrorCruncher.js
@@ -1,59 +1,83 @@
 var ErrorCruncher = {
 
-helloworld: function(){
-    alert('hello world!');
-},
+    helloworld: function () {
+        alert('hello world!');
+    },
 
-// pies/doughnuts
-// TODO: ErrorsByType
-// TODO: ErrorsByUser
-// TODO: ErrorsByDistrict
+    /**
+     * 
+     * @returns {Array}
+     */
+    generateDummyErrors: function () {
 
-// line
-// TODO: ErrorsByDay
-// TODO: ErrorsByMonth
-// TODO: ErrorsByHour? maybe
+        var count = 10000;
+        var errors = [];
 
+        for (let i = 0; i < count; i++) {
+            var error =
+            {
+                "errorId": -1,
+                "Application": "defaultapplication",
+                "Host": "defaulthost",
+                "Type": "defaulttype",
+                "Source": "defaultsource",
+                "Message": "defaultmessage",
+                "User": "defaultuser",
+                "StatusCode": "defaultstatuscode",
+                "TimeUTC": new Date(),
+                "Sequence": -1,
+                "AllXML": "defaultAllXML"
+            };
 
-showChart: function(canvasid, chartType){
-    var ctx = document.getElementById(canvasid).getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: chartType, // bar, line, pie
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
+            errors.push(error);
         }
-    });
-}
+
+        return errors;
+    },
+
+    // pies/doughnuts
+    // TODO: ErrorsByType
+    /**
+     * TODO
+     * @param {Array} errors 
+     */
+    errorsByType: function (errors) {
+
+    },
+    // TODO: ErrorsByUser
+    /**
+     * TODO
+     * @param {Array} errors 
+     */
+    errorsByUser: function (errors) {
+
+    },
+    // TODO: ErrorsByDistricts
+    /**
+     * TODO
+     * @param {Array} errors 
+     */
+    errorsByHost: function (errors) {
+
+    },
+
+    // line
+    // TODO: ErrorsByDay
+    errorsByDay: function (errors) {
+
+    },
+    // TODO: ErrorsByMonth
+    /**
+     * TODO
+     * @param {Array} errors 
+     */
+    errorsByMonth: function (errors) {
+
+    },
+    // TODO: ErrorsByHour? maybe
+    errorsByHour: function (errors) {
+
+    }
 
 
 
