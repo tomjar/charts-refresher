7789876d46cfba1aa08954bd875f24d768d266b4
Thomas Reese <cyclemadmot@gmail.com>
2020-04-12 09:59:02 -0500

    basic logic built

--- ErrorCruncher.js
+++ ErrorCruncher.js
@@ -42,7 +42,20 @@ var ErrorCruncher = {
      * @param {Array} errors 
      */
     errorsByType: function (errors) {
+        const types = [...new Set(errors.map(item => item.Type))];
 
+        var labelAndCountsArr = [];
+        for(let i = 0; i < types.length; i++){
+            let distinct = types[i];
+
+            let count = errors.filter(error => error.Type === distinct).length;
+            labelAndCountsArr.push({"label": distinct, "count":count });
+        }
+
+        var labels = labelAndCountsArr.map(item => item.label);
+        var counts = labelAndCountsArr.map(item => item.count);
+        
+        return {"labels":labels,"data": counts};
     },
     // TODO: ErrorsByUser
     /**
@@ -50,7 +63,20 @@ var ErrorCruncher = {
      * @param {Array} errors 
      */
     errorsByUser: function (errors) {
+        const users = [...new Set(errors.map(item => item.User))];
 
+        var labelAndCountsArr = [];
+        for(let i = 0; i < users.length; i++){
+            let distinct = users[i];
+
+            let count = errors.filter(error => error.User === distinct).length;
+            labelAndCountsArr.push({"label": distinct, "count":count });
+        }
+
+        var labels = labelAndCountsArr.map(item => item.label);
+        var counts = labelAndCountsArr.map(item => item.count);
+        
+        return {"labels":labels,"data": counts};
     },
     // TODO: ErrorsByDistricts
     /**
@@ -58,7 +84,20 @@ var ErrorCruncher = {
      * @param {Array} errors 
      */
     errorsByHost: function (errors) {
+        const hosts = [...new Set(errors.map(item => item.Host))];
 
+        var labelAndCountsArr = [];
+        for(let i = 0; i < hosts.length; i++){
+            let distinct = hosts[i];
+
+            let count = errors.filter(error => error.Host === distinct).length;
+            labelAndCountsArr.push({"label": distinct, "count":count });
+        }
+
+        var labels = labelAndCountsArr.map(item => item.label);
+        var counts = labelAndCountsArr.map(item => item.count);
+        
+        return {"labels":labels,"data": counts};
     },
 
     // line
